cd6ec5a01835c9c3fbb850c89f0f3057148d64f0
Daniel Felin <felinamaral@gmail.com>
2022-12-27 16:21:07 -0300

    update

---
+++ CursoJS/ex-cursoJS/ex008.js
@@ -0,0 +1,10 @@
+var vel = 100
+var velmax = 80
+
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+
+if(vel<velmax){
+    console.log(`Você está dentro do limite. Diriga com cuidado`)
+} else{
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+}
